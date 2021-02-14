3efeab1e952a24b1f0be11421a6c2c61c6e6ddea
praveennick <praveennick1@gmail.com>
2021-02-14 14:55:57 +0530

    added usermodel.js

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+var mongoose=require('mongoose');
+
+var Schema= mongoose.Schema;
+
+var UserSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required:true
+        },
+        password:{
+            type: String,
+            required:true,
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        mobile:{
+            type:Number,
+            required:true
+        }
+    }
+)
+
+module.exports = mongoose.model('user',UserSchema);
