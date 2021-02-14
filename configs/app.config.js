02dd2c5a2af654553eb6c4817a72eae3f3ca0b68
praveennick <praveennick1@gmail.com>
2021-02-14 14:56:43 +0530

    added mongourl and secret key

---
+++ configs/app.config.js
@@ -0,0 +1,7 @@
+exports.config={
+    PORT:9478,
+    MONGO_URL:"mongodb+srv://praveen:praveen@ecommerce.mnsal.mongodb.net/ecommerce?retryWrites=true&w=majority",
+    SECRET_KEY:"kaghsjkd"
+}
+
+//mongodb+srv://praveen:praveen@ecommerce.mnsal.mongodb.net/ecommerce?retryWrites=true&w=majority
