ca92c0f9f0eb3fae771bbc4a7eac89fb48f5d8d9
praveennick <praveennick1@gmail.com>
2021-02-16 16:17:03 +0530

    added some error messages

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -24,13 +24,16 @@ exports.allProducts= function(request,response){
     })
 }
 
-exports.productById=function(request,response){
+exports.getProductById=function(request,response){
     var pid=request.params.pid
     ProductModel.findOne({pid:pid},function(err,docs){
         if(err){
             response.send({err:err.message})
+        }
+        if(docs){
+            response.send(docs)
         }else{
-            response.send(docs);
+            response.send({message:"product "+pid+" not found!"})
         }
     })
 }
@@ -39,9 +42,16 @@ exports.deleteProduct=function(request,response){
     var pid= request.params.pid;
     ProductModel.deleteOne({pid:pid},function(err,docs){
         if(err){
-            response.send({error:err.message})
-        }else{
-            response.send(docs)
+            response.send({error:err.message,message:"hello"});
+            return;
+        }
+        if(docs){
+            if(docs.deletedCount<=0){
+                response.send({message:"product "+pid+" not found!"})
+            }else{
+                response.send({message:"product "+pid+" deleted successfully!"})
+            }
+            
         }
     })
 }
