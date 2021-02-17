51c98b5984463fa5808bc16f010b44835f1379c6
praveennick <praveennick1@gmail.com>
2021-02-17 15:56:18 +0530

    changed mongo url

--- configs/app.config.js
+++ configs/app.config.js
@@ -1,7 +1,7 @@
 exports.config={
     PORT:9478,
-    // MONGO_URL:"mongodb+srv://praveen:praveen@ecommerce.mnsal.mongodb.net/ecommerce?retryWrites=true&w=majority",
-    MONGO_URL:"mongodb+srv://sai:sai@ecommerce.obdec.mongodb.net/7amecommerce?retryWrites=true&w=majority",
+    MONGO_URL:"mongodb+srv://praveen:praveen@ecommerce.mnsal.mongodb.net/ecommerce?retryWrites=true&w=majority",
+    // MONGO_URL:"mongodb+srv://sai:sai@ecommerce.obdec.mongodb.net/7amecommerce?retryWrites=true&w=majority",
     SECRET_KEY:"kaghsjkd"
 }
 
