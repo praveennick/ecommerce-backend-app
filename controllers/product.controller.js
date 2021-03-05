04c05fc27d74a49f9e122e0eb63d7bc934738b61
praveennick <praveennick1@gmail.com>
2021-03-05 13:49:00 +0530

    added some apis

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,12 +2,14 @@ var ProductModel = require('../models/product.model')
 
 exports.addProduct=function(request,response){
     var product = request.body;
+    console.log("product",product)
     var newProduct = new ProductModel(product);
 
     newProduct.save(function(err,docs){
         if(err){
             response.send({status:false,message:err.message})
         }
+        console.log("docs",docs);
         if(docs._id){
             response.send({status:true,message:"Product added successfully!"});
         }
--- models/user.model.js
+++ models/user.model.js
@@ -1,30 +1,27 @@
-var mongoose=require('mongoose');
+var mongoose = require("mongoose");
 
-var Schema= mongoose.Schema;
+var Schema = mongoose.Schema;
 
-var UserSchema = new Schema(
-    {
-        username:{
-            type: String,
-            required:true
-        },
-        password:{
-            type: String,
-            required:true,
-        },
-        email:{
-            type:String,
-            required:true
-        },
-        mobile:{
-            type:Number,
-            required:true
-        },
-        role:{
-            type: String,
-            default: "ROLE_CUSTOMER"
-        }
-    }
-)
+var UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+    
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  role: {
+    type: String,
+    default: "ROLE_CUSTOMER",
+  },
+});
 
-module.exports = mongoose.model('user',UserSchema);
+module.exports = mongoose.model("user", UserSchema);
