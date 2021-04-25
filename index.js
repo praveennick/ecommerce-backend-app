237322a4e7414d92dbc91cee01a53cce1e3508ea
praveennick <praveennick1@gmail.com>
2021-04-25 22:19:08 +0530

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors= require('cors');
 
+const PORT = process.env.PORT || 9478;
+
 var app = express();
 
 var Config= require('./configs/app.config');
@@ -10,16 +12,24 @@ var productRouter= require('./routes/product.route');
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+  
 app.use('/user',userRouter);
 app.use('/admin',productRouter);
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to Ecommerce Backend Application!");
+    res.send("Welcome to Ecommerce Backend Application");
     
 })
 
 DB.connect();
 
-app.listen(Config.config.PORT,function(){
-    console.log("Server Started! on port-no:"+Config.config.PORT);
+// app.listen(Config.config.PORT,function(){
+//     console.log("Server Started! on port-no:"+Config.config.PORT);
+// })
+app.listen(PORT,function(){
+    console.log("Server Started! on port-no:");
 })
