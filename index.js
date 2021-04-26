a107df8d38de4cc7c36f071ab6112be6d74fba1f
praveennick <praveennick1@gmail.com>
2021-04-26 22:52:13 +0530

    changes

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
     next();
   });
   
@@ -22,11 +27,7 @@ app.use('/admin',productRouter);
 
 app.get('/',(req,res)=>{
     res.send("Welcome to Ecommerce Backend Application");
-    res.setHeader("Access-Control-Allow-Origin", "*")
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
-res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+    
 })
 
 DB.connect();
