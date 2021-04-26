15082b5a6bd8d2ebb3a905527c7e0a422daf31be
praveennick <praveennick1@gmail.com>
2021-04-26 22:44:04 +0530

    some change

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ app.use('/admin',productRouter);
 
 app.get('/',(req,res)=>{
     res.send("Welcome to Ecommerce Backend Application");
-    
+    res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 })
 
 DB.connect();
